b33b74a6260c448ebbe80aaa4393915bd59e9d3f
mkmtst <a@gmail.com>
2018-03-30 13:57:31 +0300

    First

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+<html>
+<head>
+    <title>Фотооппараты</title>   
+<meta charset="UTF-8">
+    
+    
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="main1.css"> 
+    <link rel="stylesheet" href="fontawesome-free-5.0.8/web-fonts-with-css/css/fontawesome-all.css">
+
+</head>
+   
+<body>
+      <div class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+    <a href ="#" class="navbar-brand" style="color:#20B2AA">NIKON SHOP</a>
+                </div>
+               <div class="navbar-collapse collapse"> 
+                
+                <ul class = "nav navbar-nav navbar-right">            
+        <li class="active"> <a href="index31.html" target="_blank"> Главная</a></li> 
+    <li tyle="color=#EE82EE"><a href="onikon.html" target="_blank">O Nikon </a> </li> 
+        <li style="color=#EE82EE"> <a href="6.html" target="_blank"> Каталог</a></li> 
+        <li style="color=#EE82EE"> <a href="7.html" target="_blank"> Контакты</a></li> 
+                 <i class="fas fa-search"></i>   <input type="text" >
+            </ul>
+                </div>
+            </div>
+    </div>
+    </body>
+</html>
